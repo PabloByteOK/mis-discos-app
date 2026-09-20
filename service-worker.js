c33cbad343428e2afda22f7a96259935feb6916d
PabloByteOK <pablodr80@gmail.com>
2026-09-20 09:29:51 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mis-discos-v1';
+const CACHE_NAME = 'mis-discos-v3';
 const ASSETS = [
     './',
     './index.html',
@@ -27,6 +27,14 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then(cached => cached || fetch(event.request))
+        fetch(event.request)
+            .then(response => {
+                if (response.ok) {
+                    const clone = response.clone();
+                    caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+                }
+                return response;
+            })
+            .catch(() => caches.match(event.request))
     );
 });
